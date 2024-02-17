64b4ae37e21daf2913da752a268fe13a485f6240
Matthew Holden <matthew.holden86@gmail.com>
2024-02-16 23:03:49 -0500

    Initial Commit

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,19 +1,11 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
+            
+class HelloCdkStack(cdk.Stack):
 
-class HelloCdkStack(Stack):
-
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "HelloCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        bucket = s3.Bucket(
+            self, "PhotoBucket-MJH140", versioned=True, encryption=s3.BucketEncryption.S3_MANAGED
+        )
